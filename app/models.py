96d439a1b56026e921e351c7b4a01e2f0cbdf25d
dev-owdenmag <mag.owden@gmail.com>
2025-01-14 21:58:55 +0000

    Models and Configurations

---
+++ app/models.py
@@ -0,0 +1,8 @@
+class Participant:
+    def __init__(self,first_name, last_name, phone, email, occupation, company):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone = phone
+        self.email = email
+        self.occupation = occupation
+        self.company = company
---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    SECRET_KEY = 'c4d41bc019aeb2498f513d9668347a9adc5f9f61cca1bd834cc1f695c7c9ea43'
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'Commando1+'
+    MYSQL_DB = 'eventify'
+    MYSQL_CURSORCLASS = 'DictCursor()'
