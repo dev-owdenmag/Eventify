4f0713cf2b83f3013b14dd7519f2f607852065a7
dev-owdenmag <mag.owden@gmail.com>
2025-01-15 12:02:20 +0000

    Styling

--- app/__init__.py
+++ app/__init__.py
@@ -7,4 +7,7 @@ app.config.from_object(Config)
 
 mysql = MySQL(app)
 
+# Add `enumerate` to the Jinja environment
+app.jinja_env.globals.update(enumerate=enumerate)
+
 from app import routes  # Import routes to register them
